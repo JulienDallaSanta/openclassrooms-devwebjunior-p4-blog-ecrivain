fcec79894266edd26ec6166b2d74d898322bc505
juliendallasanta <julien.dallasanta@gmail.com>
2020-11-30 18:19:49 +0100

    maj error_reporting & rooting

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL & ~E_NOTICE);
+    error_reporting(E_ALL);
 
     spl_autoload_register(function ($class_name) {
         include __DIR__.'/'.str_replace('\\','/',$class_name ). '.php';
     });
 
     use Controllers\PageController;
+    use Controllers\ChapterController;
 
     $rawPath = $_SERVER['REQUEST_URI'];
     $path = explode("/", $rawPath);
@@ -20,10 +21,10 @@
         PageController::printBiography();
     }
     if($path[0] == 'blog' || $path[0] == 'chapitres' || $path[0] == 'chapters'){
-        PageController::printBlog();
+        ChapterController::printBlog();
     }
     if($path[0] == 'chapitre' || $path[0] == 'chapter'){
-        PageController::printChapter();
+        ChapterController::printChapter();
     }
     if($path[0] == 'admin'){
         PageController::printAdmin();
