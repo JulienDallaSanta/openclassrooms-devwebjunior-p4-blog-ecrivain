5bfbe002f419f0ed7bbe7fa9303e19d0668a3ae4
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-01 20:20:28 +0100

    maj #helloAdmin

--- Views/header.php
+++ Views/header.php
@@ -3,8 +3,25 @@
         <span>dark mode</span>
         <input type="checkbox" name=""id="darkModeButton">
     </div>
+    <?php
+        if(isset($_SESSION['username'])){
+            ?>
+            <span id="helloAdmin">Bonjour <?= $_SESSION['username']?> !</span>
+            <?php
+        }
+    ?>
+
     <div id="connexionButton">
-        <span id="helloAdmin"></span>
-        <a id="connexionLink" data-title="Se connecter">Se connecter</a>
+        <?php
+            if(!isset($_SESSION['username'])){
+                ?>
+                <a id="connexionLink" data-title="Se connecter">Se connecter</a>
+                <?php
+            } else{
+                ?>
+                <a id="disconnectLink" data-title="Se déconnecter">Se déconnecter</a>
+                <?php
+            }
+        ?>
     </div>
 </div>
--- Views/menu.php
+++ Views/menu.php
@@ -6,4 +6,11 @@
     <li><a href="\home"><i class="icon fa fa-home fa-2x"></i><span> Accueil</span></a></li>
     <li><a href="\biography"><i class="icon fas fa-portrait fa-2x"></i><span> Biographie</span></a></li>
     <li><a href="\blog"><i class="icon fa fa-blog fa-2x"></i><span> Blog</span></a></li>
+    <?php
+    if(isset($_SESSION['grade']) && $_SESSION['grade']==='admin'){
+        ?>
+        <li><a href="/admin"><i class="icon fa fa-crown fa-2x"></i><span> Admin</span></a></li>
+        <?php
+    }
+    ?>
 </ul>
