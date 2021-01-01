89626f1ab5a7a5650ad498a1dd2684a0fe772ceb
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-01 20:19:58 +0100

    maj favicon

--- Views/head.php
+++ Views/head.php
@@ -10,7 +10,7 @@
 <!--style-->
 <link rel="stylesheet" type="text/css" href="/Public/css/style.css">
 <!--favicon-->
-<link rel="shortcut icon" type="img/png" href=""/>
+<link rel="shortcut icon" type="img/png" href="/Public/images/logojf.png"/>
 <!--Roboto-->
 <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 <!--Merriweather-->
