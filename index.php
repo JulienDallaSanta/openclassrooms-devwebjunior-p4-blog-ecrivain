834e719aec994066a0b5b7cd16b99eee730ef990
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-01 20:17:23 +0100

    path api/user/login-logout + maj returns & 404

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+    session_start();
+
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
@@ -11,40 +13,45 @@
     use Controllers\PageController;
     use Controllers\ChapterController;
     use Controllers\CommentController;
+    use Controllers\UserController;
 
     $rawPath = $_SERVER['REQUEST_URI'];
     $path = explode("/", $rawPath);
     array_shift($path);
 
-    if($path[0] == '404'){
-        PageController::print404();
-    }
     if($path[0] == 'home' || $path[0] == 'accueil' || $path[0] == ''){
-        //PageController::printHome();
-        ChapterController::printLastChapters();
+        return ChapterController::printLastChapters();
     }
     if($path[0] == 'biographie' || $path[0] == 'bio' || $path[0] == 'biography'){
-        PageController::printBiography();
+        return PageController::printBiography();
     }
     if($path[0] == 'blog' || $path[0] == 'chapitres' || $path[0] == 'chapters'){
-        ChapterController::printBlog();
+        return ChapterController::printBlog();
     }
     if($path[0] == 'chapitre' || $path[0] == 'chapter'){
         $chapterId = $path[1];
         if(isset($chapterId) && is_numeric($chapterId) && ($chapterId > 0)){
             $exist = ChapterController::exists($chapterId);
             if($exist != false){
-                ChapterController::printChapter($chapterId);
-            } else{
-                $path[0] == '404';
+                return ChapterController::printChapter($chapterId);
             }
-        }else{
-            $path[0] == '404';
         }
     }
     if($path[0] == 'admin'){
-        ChapterController::printAdmin();
+        return ChapterController::printAdmin();
     }
-
-
+    if($path[0] == 'api'){
+        header('Content-Type: application/json');
+        if($path[1] == 'user'){
+            if($path[2] == 'login'){
+                return UserController::login();
+            }
+            if($path[2] == 'logout'){
+                return UserController::logout();
+            }
+        }
+    }
+    http_response_code(404);
+    header('Content-Type: text/html; charset=utf-8');
+    return PageController::print404();
 ?>
