79cd6dc44eea47615dc6b63f08565fcd8cd5f80d
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-04 19:26:12 +0100

    create message function

--- Controllers/UserController.php
+++ Controllers/UserController.php
@@ -40,10 +40,51 @@ class UserController extends Controller{
             echo(json_encode($apiResponse['JSON']));
         }
     }
+
     static function logout(){
         $_SESSION['username'] = null;
         $_SESSION['grade'] = null;
         session_destroy();
+    }
+
+    static function message(){
+        if( isset($_POST['name']) && isset($_POST['firstname']) && isset($_POST['email']) && isset($_POST['object']) && isset($_POST['message']) ){
+            $regex = '/^[^\W][a-zA-Z0-9\-\._]+[^\W]@[^\W][a-zA-Z0-9\-\._]+[^\W]\.[a-zA-Z]{2,6}$/';
+            if(preg_match($regex, $_POST['email'])){ // Si l'email est valide...
+                $_SESSION['name'] = htmlspecialchars($_POST['name']);
+                $_SESSION['firstname'] = htmlspecialchars($_POST['firstname']);
+                $_SESSION['email'] = $_POST['email'];
+                $_SESSION['object'] = htmlspecialchars($_POST['object']);
+                $_SESSION['message'] = htmlspecialchars($_POST['message']);
+                $apiResponse = [
+                    'JSON'=> [
+                        'message' => 'OK'
+                    ],
+                    'code'=> 200
+                ];
+                http_response_code($apiResponse['code']);
+                echo(json_encode($apiResponse['JSON']));
+            }else{
+                $apiResponse = [
+                    'JSON'=> [
+                        'error' => 'Invalid email'
+                    ],
+                    'code'=> 401
+                ];
+                http_response_code($apiResponse['code']);
+                echo(json_encode($apiResponse['JSON']));
+            }
+        }
+        else{ // if one input is empty
+            $apiResponse = [
+                'JSON'=> [
+                    'error'=> 'Missing name or firstname or email or object or message'
+                ],
+                'code'=> 400
+            ];
+            http_response_code($apiResponse['code']);
+            echo(json_encode($apiResponse['JSON']));
+        }
     }
 }
 
