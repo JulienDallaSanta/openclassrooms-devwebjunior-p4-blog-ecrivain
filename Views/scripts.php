61e5548b9b3b969abac9b0e3953b3ed996f99732
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-07 19:55:22 +0100

    organizing scripts from paths

--- Views/scripts.php
+++ Views/scripts.php
@@ -1,10 +1,27 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 <script src="/Public/js/main.js"></script>
-<script src="/Public/js/localstorage.js"></script>
 <!-- TinyMCE -->
 <script src="https://cdn.tiny.cloud/1/bewiwyu29uila75t62u43sybhhii6vp30c6dfg18ud1268c7/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 <script src="/Public/js/tinymce.js"></script>
 
-<script src="/Public/js/chapitre.js"></script>
-<script src="/Public/js/admin.js"></script>
-<script src="/Public/js/livres.js"></script>
+<?php
+    $rawPath = $_SERVER['REQUEST_URI'];
+    $path = explode("/", $rawPath);
+    array_shift($path);
+    if($path[0] == 'home' || $path[0] == 'accueil' || $path[0] == ''){
+        ?>
+        <script src="/Public/js/home.js"></script>
+        <script src="/Public/js/livres.js"></script>
+    <?php
+    }
+    if($path[0] == 'chapitre' || $path[0] == 'chapter'){
+    ?>
+        <script src="/Public/js/chapitre.js"></script>
+    <?php
+    }
+    if($path[0] == 'admin'){
+    ?>
+        <script src="/Public/js/admin.js"></script>
+    <?php
+    }
+?>
