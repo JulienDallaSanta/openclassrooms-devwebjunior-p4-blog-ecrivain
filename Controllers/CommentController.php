a6c18e4cb36a758244af67fbe8ae4251f2e90b72
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-15 15:56:34 +0100

    add createComment() & delete unreport() + report()

--- Controllers/CommentController.php
+++ Controllers/CommentController.php
@@ -35,19 +35,19 @@ class CommentController extends Controller{
 
     // SETTERS
     public function setId($id){
-        Comment::getId();
+        Comment::setId();
     }
 
     public function setChapter_id($chapter_id){
-        Comment::chapter_id();
+        Comment::setChapter_id();
     }
 
     public function setPseudo($pseudo){
-        Comment::getPseudo();
+        Comment::setPseudo();
     }
 
     public function setCreation_date($creation_date){
-        Comment::getCreation_date();
+        Comment::setCreation_date();
     }
 
     public function setContent($content){
@@ -55,11 +55,11 @@ class CommentController extends Controller{
     }
 
     public function setReport($report){
-        Comment::getReport();
+        Comment::setReport();
     }
 
     public function setReport_date($report_date){
-        Comment::getReport_date();
+        Comment::setReport_date();
     }
 
 
@@ -68,25 +68,63 @@ class CommentController extends Controller{
         return $_VIEW['newComment'] = Comment::addComment($comment);
     }
 
-    public function report(){
-        $comment = new Comment();
-        if(!empty($_GET['comment']) && !empty($_GET['chapter']) && $_GET['report'] == 1){
-            $newComment = new Comment(['id' => $_GET['comment']]);
-            $comment->report($newComment);
-            header('Location: index.php?action=view&id=' .$_GET['chapter'] . '#comments');
-            exit();
+    static function createComment(){
+        if( isset($_POST['pseudo']) && isset($_POST['comment']) && isset($_POST['chapterId']) ){
+            $newComment['pseudo'] = htmlspecialchars($_POST['pseudo']);
+            $newComment['content'] = htmlspecialchars($_POST['comment']);
+            $rawPath = $_SERVER['REQUEST_URI'];
+            $path = explode("/", $rawPath);
+            array_shift($path);
+            $_POST['chapter_id'] = $path[1];
+            var_dump($path[1]);
+            $newComment['chapter_id'] = $_POST['chapter_id'];
+            $apiResponse = [
+                'JSON'=> [
+                    'message' => 'OK'
+                ],
+                'code'=> 200
+            ];
+            http_response_code($apiResponse['code']);
+            echo(json_encode($apiResponse['JSON']));
+            CommentController::addComment($newComment);
+        }else{ // if one input is empty
+            $apiResponse = [
+                'JSON'=> [
+                    'error'=> 'Missing pseudo or comment'
+                ],
+                'code'=> 400
+            ];
+            http_response_code($apiResponse['code']);
+            echo(json_encode($apiResponse['JSON']));
         }
     }
 
-    public function unreport(){
-        $comment = new Comment();
-        if(!empty($_GET['comment']) && !empty($_GET['chapter']) && $_GET['report'] == 1){
-            $newComment = new Comment(['id' => $_GET['comment']]);
-            $comment->unreport($newComment);
-            header('Location: index.php?action=view&id=' .$_GET['chapter'] . '#comments');
-            exit();
-        }
+    /**
+     * @param int $commentId
+     */
+    static function report($reportId){
+        $apiResponse = [
+            'JSON'=> [
+                'message' => 'OK'
+            ],
+            'code'=> 200
+        ];
+        http_response_code($apiResponse['code']);
+        echo(json_encode($apiResponse['JSON']));
+        $report['reportId'] = Comment::setReport($reportId);
+        $report['reportDate'] = Comment::setReport_date($report_date);
+        return $report;
     }
+
+    // public function unreport(){
+    //     $comment = new Comment();
+    //     if(!empty($_GET['comment']) && !empty($_GET['chapter']) && $_GET['report'] == 1){
+    //         $newComment = new Comment(['id' => $_GET['comment']]);
+    //         $comment->unreport($newComment);
+    //         header('Location: index.php?action=view&id=' .$_GET['chapter'] . '#comments');
+    //         exit();
+    //     }
+    // }
 
     static function getComments($chapter_id){
         $comments = Comment::getComments($chapter_id);
