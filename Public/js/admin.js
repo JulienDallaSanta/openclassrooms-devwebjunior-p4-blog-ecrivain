49ef9b25cdf79d669088fa7f80540b4868b390bd
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-15 15:59:26 +0100

    maj saveChapter()

--- Public/js/admin.js
+++ Public/js/admin.js
@@ -26,6 +26,21 @@ function statsIncrement(container, value, valMax, intervalId, intervalDuration){
 // Chapters management
 function saveChapter(){
     $("#chapterSubmit").on('click', function(){
+        tinyMCE.triggerSave();//Calls the save method on all editor instances in the collection.
+        let title = $("#chapterContent_ifr");
+        $(".page").prepend($(`
+            <div id="chapterModal">
+                <div id="chapterModalContent">
+                    <i id="chapterModalClose" class="fas fa-times-circle" onclick="closeChapterModal(event)"></i>
+                    <h3>confirmer la publication du chapitre</h3>
+                    <h5>Titre : ${localStorage.getItem('title')}</h5>
+                    <p>Texte : ${localStorage.getItem('content')}</p>
+                    <img>email : ${localStorage.getItem('chapter_image')}</img>
+                    <span>objet : ${localStorage.getItem('object')}</span>
+                    <p>message : ${localStorage.getItem('message')}</p>
+                </div>
+            </div>
+        `));
         let chapter = $(".tinymce").val();
         localStorage.setItem('chapter', chapter);
         saveToLocalStorage(chapter);
