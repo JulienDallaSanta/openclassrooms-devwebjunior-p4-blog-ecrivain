5a91e210a66265ac9fd6da7aca90929342e5919c
juliendallasanta <julien.dallasanta@gmail.com>
2021-01-30 18:49:58 +0100

    + chapterPreviewContainer

--- Views/admin.php
+++ Views/admin.php
@@ -4,26 +4,26 @@
     <div id="adminPageContent">
         <h1>Bienvenue, M. Forteroche !</h1>
         <div id="adminFuncSelect">
-            <a id="statSite" class="adminFuncSelectA">
+            <a id="statSite" class="adminFuncSelectA" href="#seoStats">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                SEO
+                Statistiques
             </a>
-            <a id="chapterAdmin" class="adminFuncSelectA">
+            <a id="chapterAdmin" class="adminFuncSelectA" href="#chaptersManagement">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                Gestion des chapitres
+                Chapitres
             </a>
-            <a id="commentsAdmin" class="adminFuncSelectA">
+            <a id="commentsAdmin" class="adminFuncSelectA" href="#commentsManagement">
                 <span></span>
                 <span></span>
                 <span></span>
                 <span></span>
-                Gestion des commentaires
+                Commentaires
             </a>
         </div>
         <div id="seoStats" class="AdminSection">
@@ -32,7 +32,7 @@
                     <h3>Nombre de sessions :</h3>
                     <div>
                         <span id="sessionsNumber" class="seoData"></span>
-                        <span id="sessionsNumberSpan">Visites sur le site</span>
+                        <span id="sessionsNumberSpan" class="seoSpan">Visites sur le site</span>
                     </div>
                 </div>
             </div>
@@ -49,9 +49,9 @@
                     <h3>Durée de session :</h3>
                     <div>
                         <p id="sessionLengthNumber">
-                            <span id="sessionLengthNumberH" class="seoData">00</span>h
-                            <span id="sessionLengthNumberM" class="seoData">35</span>m
-                            <span id="sessionLengthNumberS" class="seoData">27</span>s
+                            <span id="sessionLengthNumberH" class="seoData">00</span><span class="seoSpan">h</span>
+                            <span id="sessionLengthNumberM" class="seoData">35</span><span class="seoSpan">m</span>
+                            <span id="sessionLengthNumberS" class="seoData">27</span><span class="seoSpan">s</span>
                         </p>
                     </div>
                 </div>
@@ -85,6 +85,115 @@
             </div>
         </div>
         <div id="chaptersManagement" class="AdminSection">
+
+            <h2>Gestion des chapitres :</h2><br/>
+            <div id="chaptersManagementContent">
+                <table id="chaptersManagementTable">
+                    <tr>
+                        <th class="chaptersManagementTableTh">Aperçu</th>
+                        <th class="chaptersManagementTableTh">Chapitre</th>
+                        <th class="chaptersManagementTableTh">Titre</th>
+                        <th class="chaptersManagementTableTh">Crée le</th>
+                        <th class="chaptersManagementTableTh">Statut</th>
+                        <th class="chaptersManagementTableTh">Sélectionner</th>
+                    </tr>
+                    <?php
+                    foreach ($_VIEW['allChapters'] as $chapter){
+                    ?>
+                        <tr class="chapterLine" data-id="<?= $chapter['id']; ?>">
+                            <td><div class="chapterPreviewSelect"></div></td>
+                            <td><?= $chapter['id']; ?></td>
+                            <td><?= $chapter['title']; ?></td>
+                            <td><?= $chapter['creation_date']; ?></td>
+                            <td><?php
+                            if($chapter['published'] == 1){
+                                ?>publié le <?= $chapter['published_date'];
+                            }elseif($chapter['deleted'] == 1){
+                                ?>supprimé le <?= $chapter['published_date'];
+                            }elseif($chapter['published'] == 0 && $chapter['deleted'] == 0){
+                                ?>Sauvegardé le <?php echo $chapter['creation_date'];
+                            }?>
+                            </td>
+                            <td>
+                                <div class="chaptersActionsContainer">
+                                    <?php
+                                    if($chapter['published'] == 1){
+                                    ?>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-edit chaptersActionsIcons"></i>
+                                        <span>Modifier</span>
+                                    </div>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-trash-alt chaptersActionsIcons"></i>
+                                        <span>Supprimer</span>
+                                    </div>
+                                    <?php
+                                    }elseif($chapter['published'] == 0 && $chapter['deleted'] == 0){
+                                    ?>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-newspaper chaptersActionsIcons"></i>
+                                        <span>Publier</span>
+                                    </div>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-edit chaptersActionsIcons"></i>
+                                        <span>Modifier</span>
+                                    </div>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-trash-alt chaptersActionsIcons"></i>
+                                        <span>Supprimer</span>
+                                    </div>
+                                    <?php
+                                    }elseif($chapter['deleted'] == 1){
+                                    ?>
+                                    <div class="chaptersActionsContent">
+                                        <i class="fas fa-trash-restore chaptersActionsIcons"></i>
+                                        <span>Restaurer</span>
+                                    </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            </td>
+
+                        </tr>
+                    <?php
+                    }
+                    ?>
+                </table>
+
+                <?php
+                foreach ($_VIEW['allChapters'] as $chapter){
+                ?>
+                <div class="chapterPreviewContainer" data-id="<?php echo $chapter['id']?>">
+                    <i class="fas fa-caret-left"></i>
+                    <div class="chapterpreview">
+                        <h3 class="previewh3">Aperçu du chapitre <?php echo $chapter['id']?> :</h3>
+                        <div class="chapterpreviewContent">
+                            <h4 class="chapterPreviewTitle"><?= $chapter['title'] ?></h4>
+                            <h5 class="chapterPreviewStatus">
+                                <em>
+                                <?php
+                                if($chapter['published'] == 1){
+                                    ?>publié le <?= $chapter['published_date'];
+                                }elseif($chapter['deleted'] == 1){
+                                    ?>supprimé le <?= $chapter['published_date'];
+                                }elseif($chapter['published'] == 0 && $chapter['deleted'] == 0){
+                                    ?>Sauvegardé le <?php echo $chapter['creation_date'];
+                                }?>
+                                </em>
+                            </h5>
+                            <div  class="chapterPreviewTextImage">
+                                <img class="chapterPreviewImage" src="<?= $chapter['chapter_image'] ?>">
+                                <div class="chapterPreviewText"><?= $chapter['content'] ?></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+
             <div id="addChapterCallToAction"><p>Ajouter un chapitre</p> <i class="fas fa-chevron-down hiddenChevron"></i><i class="fas fa-chevron-up"></i></div>
             <div id="chapterEdit" style="display:none">
                 <h3>Édition d'un nouveau chapitre</h3>
@@ -120,96 +229,22 @@
                     </div>
                 </form>
             </div>
-            <h2>Gestion des chapitres :</h2>
-            <table id="chaptersManagementTable">
-                <tr>
-                    <th class="chaptersManagementTableTh">Chapitre</th>
-                    <th class="chaptersManagementTableTh">Titre</th>
-                    <th class="chaptersManagementTableTh">Crée le</th>
-                    <th class="chaptersManagementTableTh">Statut</th>
-                    <th class="chaptersManagementTableTh">Sélectionner</th>
-                </tr>
-                <?php
-                foreach ($_VIEW['allChapters'] as $chapter){
-                ?>
-                    <tr class="chapterLine">
-                        <td><?= $chapter['id']; ?></td>
-                        <td><?= $chapter['title']; ?></td>
-                        <td><?= $chapter['creation_date']; ?></td>
-                        <td><?php
-                        if($chapter['published'] == 1){
-                            ?>publié le <?= $chapter['published_date'];
-                        }elseif($chapter['deleted'] == 1){
-                            ?>supprimé le <?= $chapter['published_date'];
-                            }elseif($chapter['published'] == 0 && $chapter['deleted'] == 0){
-                            ?>Sauvegardé le <?php echo $chapter['creation_date'];
-                            }?>
-                        </td>
-                        <td>
-                            <div class="chaptersActionsContainer">
-                                <?php
-                                if($chapter['published'] == 1){
-                                ?>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-edit chaptersActionsIcons"></i>
-                                    <span>Modifier</span>
-                                </div>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-trash-alt chaptersActionsIcons"></i>
-                                    <span>Supprimer</span>
-                                </div>
-                                <?php
-                                }elseif($chapter['published'] == 0 && $chapter['deleted'] == 0){
-                                ?>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-newspaper chaptersActionsIcons"></i>
-                                    <span>Publier</span>
-                                </div>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-edit chaptersActionsIcons"></i>
-                                    <span>Modifier</span>
-                                </div>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-trash-alt chaptersActionsIcons"></i>
-                                    <span>Supprimer</span>
-                                </div>
-                                <?php
-                                }elseif($chapter['deleted'] == 1){
-                                ?>
-                                <div class="chaptersActionsContent">
-                                    <i class="fas fa-trash-restore chaptersActionsIcons"></i>
-                                    <span>Restaurer</span>
-                                </div>
-                                <?php
-                                }
-                                ?>
-                            </div>
-                        </td>
-
-                    </tr>
-                <?php
-                }
-                ?>
-
-            </table>
-
         </div>
         <div id="commentsManagement" class="AdminSection">
-            <h2>Gestion des commentaires :</h2>
-            <h3>Commentaires signalés :</h3>
-        <?php
-        $reportedComments = $_VIEW['reportedComments'];
-        if($_VIEW['numberOfReportedComments'] == 0){
-            ?>
-            <p>Aucun commentaire n'a été signalé.</p>
+            <h2>Commentaires signalés :</h2>
             <?php
-        } else{
-            ?>
+            $reportedComments = $_VIEW['reportedComments'];
+            if($_VIEW['numberOfReportedComments'] == 0){
+                ?>
+            <p>Aucun commentaire n'a été signalé.</p>
+                <?php
+                } else{
+                ?>
             <p>Il y a <?= $_VIEW['numberOfReportedComments']?> commentaires signalés :</p>
             <div id="commentsAdminBloc">
-            <?php
-            foreach($_VIEW['reportedComments'] as $reportedComment){
-            ?>
+                <?php
+                foreach($_VIEW['reportedComments'] as $reportedComment){
+                ?>
                 <div class="commentDiv commentAdmin" data-comment-id="<?php echo $reportedComment['id']; ?>">
                     <p class="namePublishDate">
                         <span class="commentId" style="display: none"><?= htmlspecialchars($reportedComment['id']); ?></span>
@@ -221,29 +256,21 @@
                     </p>
                     <p class="comment">
                         <span>Contenu du commentaire :</span></br>
-                        <?php echo nl2br(htmlspecialchars($reportedComment['content'])); ?></p>
+                        <em>"<?php echo nl2br(htmlspecialchars($reportedComment['content'])); ?>"</em>
+                    </p>
                     <div class="unreportOrDelete">
                         <a class="unreport">Annuler le signalement</a>
                         <a class="deleteComment">Supprimer le commentaire</a>
                     </div>
                 </div>
-        <?php
-            }
-        ?>
+                <?php
+                    }
+                ?>
             </div>
 
-        <?php
-        }
-        ?>
-                <p>
-                    <span class="red">Signalé le :</span>
-                    <a href="index.php?action=setCancelReport&amp;id=" title="supprimer" onclick="return(confirm('Etes-vous sûr de vouloir supprimer ce signalement ?'));">
-                        <button type="button" class=""></button>
-                    </a>
-                    <a href="index.php?action=adminCancelReport&amp;id=" title="Autoriser" onclick="return(confirm('Etes-vous sûr de vouloir autoriser ce signalement ?'));">
-                        <button type="button" class=""></button>
-                    </a>
-                </p>
+            <?php
+            }
+            ?>
         </div>
     </div>
 </section>
